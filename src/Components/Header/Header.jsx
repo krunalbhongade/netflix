674a5d8e9578760797c206f6c2d6f01a22cff9be
krunalbhongade <krunal.bhongade@gmail.com>
2023-11-01 19:00:20 +0530

    logo error removed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from "../../Logoonnetflix.png";
 import { Link } from "react-router-dom";
 import { ImSearch } from "react-icons/im";
 import logo from "../../Components/Logonetflix.png";
